69fbc8835553f17dba998484114a2040f2026679
Daniel Lopes <daniel@mswebmaker.com>
2021-08-02 17:33:42 -0300

    Implement all methods http to api class

--- MVBuilder/Core/Api.php
+++ MVBuilder/Core/Api.php
@@ -26,4 +26,53 @@ class Api extends Kernel
             }
         }
     }
+    public function head(string $path, callable $callback, callable | bool $middleware = true){
+        if($path === $this->path && $this->method === 'HEAD'){
+            if($middleware(new Request, new Response)){
+                $callback(new Request, new Response);
+            }
+        }
+    }
+    public function put(string $path, callable $callback, callable | bool $middleware = true){
+        if($path === $this->path && $this->method === 'PUT'){
+            if($middleware(new Request, new Response)){
+                $callback(new Request, new Response);
+            }
+        }
+    }
+    public function delete(string $path, callable $callback, callable | bool $middleware = true){
+        if($path === $this->path && $this->method === 'DELETE'){
+            if($middleware(new Request, new Response)){
+                $callback(new Request, new Response);
+            }
+        }
+    }
+    public function connect(string $path, callable $callback, callable | bool $middleware = true){
+        if($path === $this->path && $this->method === 'CONNECT'){
+            if($middleware(new Request, new Response)){
+                $callback(new Request, new Response);
+            }
+        }
+    }
+    public function options(string $path, callable $callback, callable | bool $middleware = true){
+        if($path === $this->path && $this->method === 'OPTIONS'){
+            if($middleware(new Request, new Response)){
+                $callback(new Request, new Response);
+            }
+        }
+    }
+    public function trace(string $path, callable $callback, callable | bool $middleware = true){
+        if($path === $this->path && $this->method === 'TRACE'){
+            if($middleware(new Request, new Response)){
+                $callback(new Request, new Response);
+            }
+        }
+    }
+    public function patch(string $path, callable $callback, callable | bool $middleware = true){
+        if($path === $this->path && $this->method === 'PATCH'){
+            if($middleware(new Request, new Response)){
+                $callback(new Request, new Response);
+            }
+        }
+    }
 }
